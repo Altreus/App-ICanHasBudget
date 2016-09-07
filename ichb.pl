bfc11951757a03303afc08c60809fca4592d46ab
Alastair McGowan-Douglas <altreus@altre.us>
2016-09-07 10:18:42 +0000

    Look I hacked a lot ok

--- ichb.pl
+++ ichb.pl
@@ -1,16 +1,44 @@
 #!/usr/bin/env perl
 use Mojolicious::Lite;
+use App::ICanHasBudget::Schema;
 
 # Documentation browser under "/perldoc"
 plugin 'PODRenderer';
+plugin 'Config';
+
+helper schema => sub {
+    my $c = shift;
+    die "No dbic config key" unless $c->config->{dbic};
+
+    state $s = App::ICanHasBudget::Schema->connect(
+        $c->config->{dbic}->{dsn},
+        $c->config->{dbic}->{user},
+        $c->config->{dbic}->{pass},
+        $c->config->{dbic}->{dbi_params}
+    );
+};
 
 get '/' => sub {
+    my $c = shift;
+    $c->redirect_to('/kitties');
+};
+
+get '/kitties' => sub {
     my $c = shift;
     $c->render(
         template => 'kitties',
         section => 'kitties',
-        kitties => [],
+        kitties => [ $c->schema->resultset('Kitty')->all ],
     );
+};
+
+post '/kitties' => sub {
+    my $c = shift;
+    $c->schema->resultset('Kitty')->create({
+        name => $c->param('new-kitty-name'),
+        type => $c->param('new-kitty-type') ? 'budget' : 'target'
+    });
+    $c->redirect_to('/kitties');
 };
 
 get '/accounts' => sub {
@@ -28,5 +56,18 @@ get '/trends' => sub {
         section => 'trends',
     );
 };
+
+get '/add_transaction' => sub {
+    my $c = shift;
+    transaction_form($c)
+};
+
+sub transaction_form {
+    my $c = shift;
+    $c->render(
+        template => 'transaction_form',
+        section => 'accounts',
+    );
+}
 
 app->start;

9ab51485113c7fd748b01a975d183835cd18df67
Alastair McGowan-Douglas <altreus@altre.us>
2016-10-09 23:03:29 +0000

    Create calendar

--- lib/App/ICanHasBudget/Controller/Schedule.pm
+++ lib/App/ICanHasBudget/Controller/Schedule.pm
@@ -1,12 +1,30 @@
 package App::ICanHasBudget::Controller::Schedule;
 use Mojo::Base qw/Mojolicious::Controller/;
+use Time::Moment;
 
 sub index {
     my $self = shift;
+
+    my $calendar = [];
+    my $today = Time::Moment->now;
+    my $first_of_month = $today->with_day_of_month(1);
+    my $next_month = $today->plus_months(1);
+    my $first_of_week = $first_of_month->with_day_of_week(1);
+
+    my $working_date = $first_of_week;
+    while ($working_date->is_before($next_month)) {
+        push @$calendar, [ map $working_date->with_day_of_week($_), 1..7 ];
+        $working_date = $working_date->plus_days(7);
+    }
+
     $self->render(
-        template => 'trends',
-        section => 'trends',
+        template => 'schedule',
+        section => 'schedule',
+        calendar => $calendar,
+        first_of_month => $first_of_month,
+        last_of_month => $first_of_month->at_last_day_of_month,
+        today => $today,
     );
-};
+}
 
 1;
